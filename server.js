e58a619c789e7861b0356a79b267c534591aa36c
Alex Tan Hong Pin <alextan220990@gmail.com>
2017-01-05 17:30:46 +0800

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+var FeedParser = require('feedparser');
+var request = require('request'); // for fetching the feed
+
+var req = request('http://rss.cnn.com/rss/money_markets.rss')
+var feedparser = new FeedParser();
+
+req.on('error', function (error) {
+  // handle any request errors
+});
+
+req.on('response', function (res) {
+  var stream = this; // `this` is `req`, which is a stream
+
+  if (res.statusCode !== 200) {
+    this.emit('error', new Error('Bad status code'));
+  }
+  else {
+    stream.pipe(feedparser);
+  }
+});
+
+feedparser.on('error', function (error) {
+  // always handle errors
+});
+
+feedparser.on('readable', function () {
+  // This is where the action is!
+  var stream = this; // `this` is `feedparser`, which is a stream
+  var meta = this.meta; // **NOTE** the "meta" is always available in the context of the feedparser instance
+  var item;
+
+  while (item = stream.read()) {
+    console.log(item.title);
+    console.log(item.description);
+    console.log(item.summary);
+    console.log(item.pubdate);
+    console.log(item.author);
+    console.log(item.image);
+    console.log(item.origlink);
+  }
+});
